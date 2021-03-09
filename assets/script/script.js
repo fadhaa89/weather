cec895637f1caa6684153850a0b5e25f2f015524
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-09 02:35:08 -0600

    current temp-humidity location is added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+ var citySearch;
  var apikey ="d028839cdc9a1c9698eac3bfe3105f91" ;
  var apicall ="http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
  var apicompleteCall = apikey + apicall;
