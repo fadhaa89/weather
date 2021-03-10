9a4d6a3b25b2aa1a46b01a6956508da797ee5a3c
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-09 20:22:14 -0600

    fontawesome link is added ,seach btn is used

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,29 @@
  var apicall ="http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
  var apicompleteCall = apikey + apicall;
  console.log (apicompleteCall);
+ 
+ function search() {
+    $("#search-button").on("click", function () {
+      citySearch = $("#search-input").val().trim();
+
+      if (citySearch === "") {
+        return;
+      }
+      $("#search-input").val("");
+      getWeather(citySearch);
+    });
+  }
+
+  function getWeather(search) {
+    var queryURL = weatherAPI + "q=" + search + units + APIkey ;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        statusCode: {
+          404: function () {
+            $("#current-forecast").hide();
+            $("#five-day-forecast-container").hide();
+            $("#error-div").show();
+          },
+        },
