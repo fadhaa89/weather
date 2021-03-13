59fd186c47573c43278e72b7e1664f6dc4d3f9cb
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-13 01:07:03 -0600

    API key is updated

---
+++ assets/script/script.js
@@ -0,0 +1,44 @@
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//request /instruction how make a request
+
+var cityName = "chicago";
+//API KEY 
+var APIkey = "d028839cdc9a1c9698eac3bfe3105f91";
+//WEATHER API as it is
+var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
+//this queryURL is as it is from the the web 
+var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
+console.log(queryURL);
+
+//var units = 
+var getWeatherIcon = "http://openweathermap.org/img/wn/";
+
+var searchHistoryArr = [];
+
+var todayWeather = {};
+// START
+
+//  function init () {
+//     console.log("Hello!")
+// }
+
+//ajax fot currentweather to get the current weather feom openwaether.
+//define the function -
+function currentWeather () {
+    $.ajax ({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        
+    })
+    
+}
+
+//as soon html loads do this things :
+//$(document).ready(function () {
+    //init();
+    //call the function by name
+    currentWeather();
+
+// });
