211609fad95417cc470fe1bbb8f27642158c3439
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-13 01:41:32 -0600

    UV api key is added

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,6 +7,7 @@ var APIkey = "d028839cdc9a1c9698eac3bfe3105f91";
 //WEATHER API as it is
 var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
 var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
+uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
 //this queryURL is as it is from the the web 
 var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
 console.log(queryURL);
@@ -53,6 +54,60 @@ function saveSearchHistory(searchInput) {
     searchHistory.push(searchInput);
     localStorage.setItem(LS_KEY, JSON.stringify(searchHistory));
 }
+
+//ADDS CITY TO LIST BOX <-----THIS FUNCTION NOT WORKING PROPERLY
+function addToCityList() {
+    if (searchHistory) {
+        searchHistory = JSON.parse(localStorage.getItem(LS_KEY));
+        $('#searchedCityList').append(searchHistory.city);
+        console.log(searchHistory);
+    }
+}
+var date;
+let currentData;
+
+
+async function getWeatherData() {
+//var paramsCurrent = "current" + "?key=" + API_KEY + "&units=i&city=";
+var uviQueryURL = uviAPI + lat + "&lon=" + lon + APIkey; // uv API KEy
+var url = wbEndPt;
+cityState = getUserInputs();
+paramsCurrent += cityState;
+url += paramsCurrent;
+await weatherNow(url, cityState);
+getForecastData();
+}
+
+function getForecastData() {
+var paramsCurrent = "forecast/daily" + "?key=" + API_KEY + "&units=i&city=" + cityState + "&days=5";
+var url = wbEndPt;
+url += paramsCurrent;
+weatherForecast(url);
+// const forecastQueryURL = "http://api.weatherbit.io/v2.0/forecast/daily?key=314f8e60676745089873728b30174cc3&units=i&city=Novato, CA&days=5";
+}
+
+
+function uvindex(uv) {
+console.log("uv is " + JSON.stringify(uv));
+if (uv < 3) {
+$('#current-uv').css('uv-low') //green
+} else if (uv > 2 && uv < 6) {
+$('#current-uv').css('uv-mod-lo') //yellow
+} else if (uv > 5 && uv < 8) {
+$('#current-uv').css('uv-mod-hi') //orange   
+} else if (uv > 7 && uv < 10) {
+$('#current-uv').css('uv-severe-lo') //red
+} else if (uv > 9) {
+$('#current-uv').css('uv-severe-hi') //purple
+} else {
+$('#current-uv').css('uv-low') //green
+}
+}
+
+//$(document).ready(function () {
+//setup();
+getWeatherData();
+//});
 
 //as soon html loads do this things :
 //$(document).ready(function () {
