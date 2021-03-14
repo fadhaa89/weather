1084bf8527cd30bd3f2abf9188998862f9050dfb
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-14 18:06:35 -0500

    eventlistner is working

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,7 +9,10 @@ var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
 var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
 uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
 //this queryURL is as it is from the the web 
-var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
+//var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
+//console.log(queryURL);
+var corsProxy = "https://cors-anywhere.herokuapp.com/";
+var queryURL = corsProxy + weatherAPI + cityName + "&appid=" + APIkey;
 console.log(queryURL);
 
 var getWeatherIcon = "http://openweathermap.org/img/wn/";
@@ -23,11 +26,13 @@ var forecastWeatherArr = [];
 var cityState;
 
 eventListeners = () => {
-  $('#search-button"').on('click', function (event) {
+  $('#search-button').on('click', function (event) {
       event.preventDefault();
-      alert("Hello!");
+      search();
+
   })
 }
+
 //ajax fot currentweather to get the current weather feom openwaether.
 //define the function -
 function currentWeather () {
@@ -44,58 +49,41 @@ function currentWeather () {
     
 }
 // search function
+//whatever in city input its a value
+
 function search() {
-    $("#search-button").on("click", function () {
-      citySearch = $("#search-input").val().trim();
+      cityName = $("#city-input").val().trim();
+      alert(cityName);// val not a string 
+    };
 
-      if (citySearch === "") {
-        return;
-      }
-      $("#search-input").val("");
-      getWeather(citySearch);
-    });
-  }
+  // get the city
+  getCity = () => {
+    console.log("Running getCity function.");
+    cityName = $('#city').val();
+    return cityName
+}
 //save the search history to local storage
-function saveSearchHistory(searchInput) {
-    searchHistory.push(searchInput);
-    localStorage.setItem(LS_KEY, JSON.stringify(searchHistory));
-}
-//ADDS CITY TO LIST BOX <-----THIS FUNCTION NOT WORKING PROPERLY
-function addToCityList() {
-    if (searchHistory) {
-        searchHistory = JSON.parse(localStorage.getItem(LS_KEY));
-        $('#searchedCityList').append(searchHistory.city);
-        console.log(searchHistory);
-    }
-}
-var date;
-let currentData;
 
-function uvindex(uv) {
-console.log("uv is " + JSON.stringify(uv));
-if (uv < 3) {
-$('#current-uv').css('uv-low') //green
-} else if (uv > 2 && uv < 6) {
-$('#current-uv').css('uv-mod-lo') //yellow
-} else if (uv > 5 && uv < 8) {
-$('#current-uv').css('uv-mod-hi') //orange   
-} else if (uv > 7 && uv < 10) {
-$('#current-uv').css('uv-severe-lo') //red
-} else if (uv > 9) {
-$('#current-uv').css('uv-severe-hi') //purple
-} else {
-$('#current-uv').css('uv-low') //green
-}
-}
 
-//$(document).ready(function () {
-//setup();
-//});
 
-//as soon html loads do this things :
-//$(document).ready(function () {
-    //init();
-    //call the function by name
-    //currentWeather();
+// let currentData;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function uvindex(uv) {
+// console.log("uv is " + JSON.stringify(uv));
+// if (uv < 3) {
+// $('#current-uv').css('uv-low') //green
+// } else if (uv > 2 && uv < 6) {
+// $('#current-uv').css('uv-mod-lo') //yellow
+// } else if (uv > 5 && uv < 8) {
+// $('#current-uv').css('uv-mod-hi') //orange   
+// } else if (uv > 7 && uv < 10) {
+// $('#current-uv').css('uv-severe-lo') //red
+// } else if (uv > 9) {
+// $('#current-uv').css('uv-severe-hi') //purple
+// } else {
+// $('#current-uv').css('uv-low') //green
+// }
+// }
+$(document).ready(function (){
+  eventListeners()
+});
