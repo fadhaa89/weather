7de3e8b93024606fe22d9f5aef3209370c5dea35
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-03-26 21:29:12 -0500

    todayWeatherApi data posting to dom

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,59 +1,60 @@
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 //https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={API key}
 //request /instruction how make a request
 // need a function that grabs the city input and plugs that into the getWeather function, and a solution for CORS errors
-var cityName = "chicago";
+var cityName;
 //API KEY 
 var APIkey = "d028839cdc9a1c9698eac3bfe3105f91";
+var todayWeatherApi = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
 //WEATHER API as it one day weather//
 var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-var lat = "41.8781";
-var lon = "87.6298";
+var lat;
+var lon;
 
 //5day 
 var forecastAPI = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${APIkey}`;
 console.log(forecastAPI);
 
 //this queryURL is as it is from the the web 
-var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
+// var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
 //console.log(queryURL);
 var corsProxy = "http://cors-anywhere.herokuapp.com/";
 var queryURL = corsProxy + forecastAPI;
 console.log(queryURL);
-
 var getWeatherIcon = "http://openweathermap.org/img/wn/";
-
 var searchHistoryArr = [];
-
 var currentWeather = {};
 var searchHistory = [];
 var searchInput = '';
-// var forecastWeatherArr = [];
+var forecast;
 var cityState;
+var todayWeather ={};
 
+function getCity(){
+cityName = $("#searchCityBar").val();
+console.log("cityName:"+ cityName)
+return cityName;
+}
 
 function getWeather () {
+ cityName = getCity();
   console.log('getWeather')
   $.ajax ({
-      url: queryURL,
+      url:`${corsProxy}api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${APIkey}`,
       method: "GET",
       headers: {
         "X-Requested-With": "XMLHttpRequest"
         }, //to handle the cors error 
   }).then(function (response) {
-      console.log(response)
-
-      forecastWeatherArr = response;
+      todayWeather = response;
       console.log('response',response);
-      lat=response.lat
-      lon=response.lon
+      lat=todayWeather.coord.lat;
+      lon=todayWeather.coord.lon;
 
-      
-      $("#temp").text(forecastWeatherArr.current.temp);//id is assigned-text function-
-      $("#humidity").text(forecastWeatherArr.current.humidity);//id is assigned-text function-
-      $("#windSpeed").text(forecastWeatherArr.current.wind_speed);//id is assigned-text function-
-      $("#uvIndex").text(forecastWeatherArr.current.uvi);//id is assigned-text function-
+      $("#temp").text(todayWeather.main.temp);//id is assigned-text function-
+      $("#humidity").text(todayWeather.main.humidity);//id is assigned-text function-
+      $("#windSpeed").text(todayWeather.wind.speed);//id is assigned-text function-
+      // $("#uvIndex").text(todayWeather.current.uvi);//id is assigned-text function-
 
 //       /////////////////////////////////////////////////////////////////////////////////
 //       for (let index = 0; index < 5; index++) { //forloop for 5days weather /
@@ -79,7 +80,9 @@ $.ajax ({
 })
 .then((response2) => {
   console.log('response2',response2)
+  forecast.list = response2;
     for (let i = 0; i < forecast.list.length; i++) {
+
         // console.log(forecast.list[i].dt_txt.split(" ")[1]);
         if (forecast.list[i].dt_txt.split(" ")[1] === "12:00:00") {
             console.log(forecast.list[i]);
@@ -99,22 +102,21 @@ $.ajax ({
 })
 
 }
-
-// search function
-//whatever in city input its a value
 function search() {
     console.log('running search function');
-    cityName = $("#city-input").val();
-    getWeather();
+   
+ 
     return cityName 
 };
-///////////////////////////////////////////////////////////////////////
 
+//   eventListeners()
 
-//save the search history to local storage
-
-
-
+  $('#search-button').on('click', function (event) {
+    console.log('search-button')
+      event.preventDefault();
+      
+      getWeather(cityName);
+  })
 // let currentData;
 
 // function uvindex(uv) {
@@ -134,10 +136,3 @@ function search() {
 // }
 // }
 // $(document).ready(function (){
-//   eventListeners()
-
-  $('#search-button').on('click', function (event) {
-    console.log('search-button')
-      event.preventDefault();
-      search();
-  })
